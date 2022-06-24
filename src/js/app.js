cdf380de3b0c787de0c2500a1475a26d25a2dac1
stephenjhs <floss.stephen@gmail.com>
2022-06-25 00:28:16 +0700

    fix audio on mobile

--- src/js/app.js
+++ src/js/app.js
@@ -14,8 +14,26 @@ const navMenuIssue = () => {
 }
 
 const malingPangsitAudio = () => {
-    const audio = new Audio("src/audios/maling-pangsit.mp3")
-    audio.play()
+    // const audio = new Audio("src/audios/maling-pangsit.mp3")
+    // audio.play()
+
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+    const source = audioCtx.createBufferSource()
+    const xhr = new XMLHttpRequest()
+    xhr.open("GET", "src/audios/maling-pangsit.mp3")
+    xhr.responseType = "arraybuffer"
+    xhr.addEventListener("load", function (r) {
+        let playsound = (audioBuffer) => {
+            let source = audioCtx.createBufferSource()
+            source.buffer = audioBuffer
+            source.connect(audioCtx.destination)
+            source.loop = false
+            source.start()
+        }
+
+        audioCtx.decodeAudioData(xhr.response).then(playsound)
+    })
+    xhr.send()
 }
 
 const init = () => {
