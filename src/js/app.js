62c6e8fabf4df028ec949fd89805614a13d4af7e
stephenjhs <floss.stephen@gmail.com>
2022-06-25 20:09:52 +0700

    update baru hapus malingpangsit dan aos

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,15 @@ const toggleEvent = () => {
     const toggleEls = document.querySelectorAll(".toggle")
     const canvasEl = document.querySelector(".canvas")
 
-    toggleEls.forEach((item) => {
-        item.addEventListener("click", () => {
+    for (const toggleEl of toggleEls) {
+        toggleEl.addEventListener("click", () => {
             canvasEl.classList.toggle("collapse")
         })
-    })
-}
-
-const malingPangsitAudio = () => {
-    const audio = new Audio("src/audios/maling-pangsit.mp3")
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 1) {
-            audio.play()
-        }
-    })
+    }
 }
 
 const init = () => {
     toggleEvent()
-    malingPangsitAudio()
-    AOS.init({
-        offset: 270,
-        once: true,
-    })
 }
 
 window.addEventListener("load", init())
